bb2f5709f754ead035b748b393081e604946a631
Deyan Yolovich <saykovac@gmail.com>
2022-03-27 23:12:45 +0200

    Commit...

--- cypress/integration/myFirstTest.js
+++ cypress/integration/myFirstTest.js
@@ -6,10 +6,11 @@ it('google test', function(){
     cy.visit('https://google.com')
 
     cy.get('.gLFyf'/*,{timeout: 6000}*/).type('Bujaka Bujaka{enter}')
-    // kopriali selektor
+    // kopirali selektor
     // cy.get('[href="https://genius.com/The-jam-going-underground-lyrics"] > .LC20lb').click()
     cy.wait(4000)
+    // 
     cy.contains('Видео').click()
-    // Chrome otvara Google na srpskom te je zato Видео
+    // Chrome otvara Google na srpskom te je zato Видео a ne Video
 
 })
